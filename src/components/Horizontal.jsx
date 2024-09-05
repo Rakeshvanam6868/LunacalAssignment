5200c86ee5c5cef0f1f03eff44504142b8074691
Rakeshvanam6868 <rakeshvanam6868@gmail.com>
2024-09-05 18:24:27 +0530

    Added the responsive

--- src/components/Horizontal.jsx
+++ src/components/Horizontal.jsx
@@ -1,6 +1,6 @@
 const Horizontal = () => {
   return (
-    <div className="w-[652px] h-[4px] xl:ml-[260px] sm:ml-[45px] lg:ml-[190px] 2xl:ml-[953px] mt-[31px] rounded-[3px] bg-[#ffffff] bg-gradient-to-b from-[#282828] shadow-[6px_6px_3px_0px_rgba(0,0,0,0.4)] to-[#F8F8F8] drop-shadow-md"></div>
+    <div className="w-[652px] h-[4px] xl:ml-[740px] sm:ml-[45px] lg:ml-[190px] 2xl:ml-[953px] mt-[31px] rounded-[3px] bg-[#ffffff] bg-gradient-to-b from-[#282828] shadow-[6px_6px_3px_0px_rgba(0,0,0,0.4)] to-[#F8F8F8] drop-shadow-md"></div>
   )
 }
 
